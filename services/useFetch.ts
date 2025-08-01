41176f3e5ba3d46deaf3fbc80905993469d04c03
Subodh <sohonisubodh@gmail.com>
2025-08-01 16:58:18 +0530

    TMDB api setup

---
+++ services/useFetch.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+
+const useFetch = <T>(fetchFuntion: ()=> Promise<T>, autoFetch = true) => {
+    const [data, setData] = useState<T | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<Error | null>(null);
+
+    const fetchData = async () => {
+        
+        try {
+            setLoading(true);
+            setError(null);
+            const result = await fetchFuntion();
+            setData(result);
+        } catch (error) {
+            //@ts-ignore
+            setError(err instanceof Error ? err : new Error('An error occurred'));
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const reset = () => {
+        setData(null);
+        setError(null);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        if (autoFetch) {
+            fetchData();
+        }
+    }, []);
+
+    return {
+        data,
+        loading,
+        error,
+        refetch:fetchData,
+        reset
+    };
+}
+
+export default useFetch
