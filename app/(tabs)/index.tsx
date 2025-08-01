20c85bcf7450a2b67f1ddd8108088f7604a84fee
Subodh <sohonisubodh@gmail.com>
2025-08-01 17:52:16 +0530

    Fetch and display movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,58 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { Link, useRouter } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
 
   const router = useRouter();
+  const {data: movies, loading: moviesLoading, error: moviesError} = useFetch(() => fetchMovies({query: ''}));
 
   return (
     <View className="flex-1 bg-primary">
       <Image source={images.bg} className="absolute z-0 w-full" />
       <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ paddingBottom: 10, minHeight: "100%" }}>
         <Image source={icons.logo} className="w-14 h-14 mx-auto mt-20 mb-5" />
-        <View className="flex-1">
-          <SearchBar 
-            onPress={() => {
-              router.push("/search");
-            }}
-            placeholder="Search for a movie"
-          />
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+        ): moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ): (
+          <View className="flex-1 mt-5">
+            <SearchBar 
+              onPress={() => {
+                router.push("/search");
+              }}
+              placeholder="Search for a movie"
+            />
+            <>
+              <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+              <FlatList 
+                data={movies}
+                renderItem={({item}) => (
+                  <MovieCard
+                    {...item}
+                  />
+                )}
+                keyExtractor={(item) => item.id.toString()}
+                numColumns={3}
+                columnWrapperStyle={{
+                  justifyContent: "flex-start",
+                  gap:20,
+                  paddingRight: 5,
+                  marginBottom: 10
+                }}
+                className="mt-2 pb-32"
+                scrollEnabled={false}
+              />
+            </>
         </View>
+        )}
+        
       </ScrollView>
     </View>
   );
--- services/useFetch.ts
+++ services/useFetch.ts
@@ -1,44 +1,39 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 
-const useFetch = <T>(fetchFuntion: ()=> Promise<T>, autoFetch = true) => {
-    const [data, setData] = useState<T | null>(null);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<Error | null>(null);
+const useFetch = <T>(fetchFunction: () => Promise<T>, autoFetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<Error | null>(null);
 
-    const fetchData = async () => {
-        
-        try {
-            setLoading(true);
-            setError(null);
-            const result = await fetchFuntion();
-            setData(result);
-        } catch (error) {
-            //@ts-ignore
-            setError(err instanceof Error ? err : new Error('An error occurred'));
-        } finally {
-            setLoading(false);
-        }
-    };
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null);
 
-    const reset = () => {
-        setData(null);
-        setError(null);
-        setLoading(false);
+      const result = await fetchFunction();
+      setData(result);
+    } catch (err) {
+      setError(
+        err instanceof Error ? err : new Error("An unknown error occurred")
+      );
+    } finally {
+      setLoading(false);
     }
+  };
 
-    useEffect(() => {
-        if (autoFetch) {
-            fetchData();
-        }
-    }, []);
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
 
-    return {
-        data,
-        loading,
-        error,
-        refetch:fetchData,
-        reset
-    };
-}
+  useEffect(() => {
+    if (autoFetch) {
+      fetchData();
+    }
+  }, []);
 
-export default useFetch
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+export default useFetch;
